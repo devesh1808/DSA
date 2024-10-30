8a3407799fbfdedcadf3c0fb88e09c82b9366e3c
devesh1808 <devesh1808@gmail.com>
2024-10-31 02:52:11 +0530

    added a more shorter code for set matrix zeros

--- Arrays/Arrays-I/leetcode 73 - Set Matrix Zeros.cpp
+++ Arrays/Arrays-I/leetcode 73 - Set Matrix Zeros.cpp
@@ -76,3 +76,33 @@ void setZeroes(vector<vector<int>> &arr)
         if (arr[i][0] == 0 || zeroVal == 0)
             arr[i][0] = 0;
     }
+
+    // a more shorter code
+
+    class Solution
+    {
+    public:
+        void setZeroes(vector<vector<int>> &matrix)
+        {
+            int rows = matrix.size(), cols = matrix[0].size();
+            bool col0 = false;
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (matrix[i][0] == 0)
+                    col0 = true;
+                for (int j = 1; j < cols; j++)
+                    if (matrix[i][j] == 0)
+                        matrix[i][0] = matrix[0][j] = 0;
+            }
+
+            for (int i = rows - 1; i >= 0; i--)
+            {
+                for (int j = cols - 1; j >= 1; j--)
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                        matrix[i][j] = 0;
+                if (col0 == true)
+                    matrix[i][0] = 0;
+            }
+        }
+    };
