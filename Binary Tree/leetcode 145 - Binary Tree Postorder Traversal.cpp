2734734c7cd268d5f0d484b8a3c2791736a22ec2
devesh1808 <devesh1808@gmail.com>
2024-11-29 23:55:39 +0530

    postorder traversal using recursion

---
+++ Binary Tree/leetcode 145 - Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,16 @@
+// Using Recursion
+
+void func(TreeNode *root, vector<int> &ans)
+{
+    if (root == NULL)
+        return;
+    func(root->left, ans);
+    func(root->right, ans);
+    ans.push_back(root->val);
+}
+vector<int> postorderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    func(root, ans);
+    return ans;
+}
