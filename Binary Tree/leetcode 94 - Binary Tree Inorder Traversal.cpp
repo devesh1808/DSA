0f1b43a3fc8fb75e1310e6b9036e8987bf731f7d
devesh1808 <devesh1808@gmail.com>
2024-11-29 23:50:20 +0530

    inorder traversal using iteration (stack)

--- Binary Tree/leetcode 94 - Binary Tree Inorder Traversal.cpp
+++ Binary Tree/leetcode 94 - Binary Tree Inorder Traversal.cpp
@@ -14,3 +14,27 @@ vector<int> inorderTraversal(TreeNode *root)
     func(root, ans);
     return ans;
 }
+
+// Using Iteration (Stack)
+
+vector<int> inorderTraversal(TreeNode *root)
+{
+    if (root == NULL)
+        return {};
+    vector<int> ans;
+    stack<TreeNode *> st;
+    TreeNode *curr = root;
+    while (!st.empty() || curr != NULL)
+    {
+        while (curr != NULL)
+        {
+            st.push(curr);
+            curr = curr->left;
+        }
+        curr = st.top();
+        st.pop();
+        ans.push_back(curr->val);
+        curr = curr->right;
+    }
+    return ans;
+}
