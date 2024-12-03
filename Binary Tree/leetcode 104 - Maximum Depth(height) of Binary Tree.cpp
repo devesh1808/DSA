eee7670e01c3551b9d8a07e55a1b98393e6189d9
devesh1808 <devesh1808@gmail.com>
2024-12-03 23:42:06 +0530

    height of binary tree iteratively (using queue)

--- Binary Tree/leetcode 104 - Maximum Depth(height) of Binary Tree.cpp
+++ Binary Tree/leetcode 104 - Maximum Depth(height) of Binary Tree.cpp
@@ -8,3 +8,40 @@ int maxDepth(TreeNode *root)
     int right = maxDepth(root->right);
     return max(left, right) + 1;
 }
+
+// iterative solution
+
+int maxDepth(TreeNode *root)
+{
+    if (!root)
+    {
+        return 0;
+    }
+
+    queue<TreeNode *> q;
+    q.push(root);
+    int depth = 0;
+
+    while (!q.empty())
+    {
+        depth++;
+        int levelSize = q.size();
+
+        for (int i = 0; i < levelSize; i++)
+        {
+            TreeNode *node = q.front();
+            q.pop();
+
+            if (node->left)
+            {
+                q.push(node->left);
+            }
+            if (node->right)
+            {
+                q.push(node->right);
+            }
+        }
+    }
+
+    return depth;
+}
