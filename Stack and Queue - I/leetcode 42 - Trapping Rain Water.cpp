21a6d13eb5614838b95ccbc3bae739d520d75428
devesh1808 <devesh1808@gmail.com>
2025-01-04 23:08:21 +0530

    using 2 pointers

--- Stack and Queue - I/leetcode 42 - Trapping Rain Water.cpp
+++ Stack and Queue - I/leetcode 42 - Trapping Rain Water.cpp
@@ -21,3 +21,33 @@ int trap(vector<int> &height)
     }
     return ans;
 }
+
+// Solution 2: Using 2 pointers
+
+int trap(vector<int> &height)
+{
+    int n = height.size();
+    int left = 0, right = n - 1;
+    int lmax = 0, rmax = 0;
+    int ans = 0;
+    while (left < right)
+    {
+        if (height[left] <= height[right])
+        {
+            if (height[left] > lmax)
+                lmax = height[left];
+            else
+                ans += (lmax - height[left]);
+            left++;
+        }
+        else
+        {
+            if (height[right] > rmax)
+                rmax = height[right];
+            else
+                ans += (rmax - height[right]);
+            right--;
+        }
+    }
+    return ans;
+}
